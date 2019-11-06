84ef4687cc982c2be333517b20a108fbc96ca819
Guilherme Lôbo <gui.wolflobo@gmail.com>
2019-11-05 22:01:37 -0300

    classes e controle de tela

---
+++ js/class.js
@@ -0,0 +1,32 @@
+class Tela {
+  constructor() {
+    $.get("views/nav.htm", function (data) {
+         $('body').prepend(data); //comando .prepend() adiciona no inicio do elemento
+         var elems = document.querySelectorAll('.sidenav');
+         var instances = M.Sidenav.init(elems);
+    });
+    this.telaPrincipal();
+  }
+
+  telaPrincipal(){
+    $.get("views/telaPrincipal.htm", function (data) {
+         $('#container').append(data);
+
+    });
+      limpaTela();
+  }
+
+  telaDeLogica(){
+    $.get("views/telaDeLogica.htm", function (data) {
+         $('#container').append(data);
+
+    });
+      limpaTela();
+  }
+
+  limpaTela(){
+     $('#container').empty();
+  }
+
+
+}
--- js/init.js
+++ js/init.js
@@ -1,10 +1,2 @@
-//inicia a nav
-$.get("views/nav.htm", function (data) {
-     $('body').prepend(data); //comando .prepend() adiciona no inicio do elemento
-     var elems = document.querySelectorAll('.sidenav');
-     var instances = M.Sidenav.init(elems);
-});
-// chama tela principal
-$.get("views/telaPrincipal.htm", function (data) {
-     $('#container').append(data);
-});
+var tela = new Tela();
+tela.telaPrincipal();
