9734f1db84ad5f69a88b05456f86f4351e6b65eb
Maicon Francisco <maicon.fr95@gmail.com>
2020-02-06 20:49:44 -0300

    Variáveis declaradas

--- app_calc_3d/www/js/index.js
+++ app_calc_3d/www/js/index.js
@@ -2,6 +2,12 @@ $( document ).ready(function() {
 $("#calc").click(function() {
   var peso = $("#peso").val();
   var tempo = $("#tempo").val();
+  var preco = $("#preco-filamento").val();
+  var consumo = $("#consumo-impressora").val();
+  var taxa = $("#taxa-energia").val();
+  var lucro = $("#lucro").val();
+  var erroTempo = $("#erro-tempo").val();
+  var erroPeso = $("#erro-peso").val();
 
   var calculo = (peso+erroPeso)*(preco/1000) + ((tempo+erroTempo)*taxaEnergia*consumo);
 
