76fb914fc05c695b861331dc5f4bca4d05bc673f
Guilherme Lôbo <gui.wolflobo@gmail.com>
2020-02-06 21:08:51 -0300

    correçoes nas variaveis

--- app_calc_3d/platforms/android/app/src/main/assets/www/js/index.js
+++ app_calc_3d/platforms/android/app/src/main/assets/www/js/index.js
@@ -2,13 +2,20 @@ $( document ).ready(function() {
 $("#calc").click(function() {
   var peso = $("#peso").val();
   var tempo = $("#tempo").val();
+  var preco = $("#preco-filamento").val();
+  var consumo = $("#consumo-impressora").val();
+  var taxaEnergia = $("#taxa-energia").val();
+  var lucro = $("#lucro").val();
+  var erroTempo = $("#erro-tempo").val();
+  var erroPeso = $("#erro-peso").val();
 
-  var calculo = peso*(0.16) + (tempo*0.8*0.4);
+ console.log("o calculo ficou assim: ("+peso+"+("+(erroPeso/100)+"*"+peso+"))*("+preco+"/1000)");
+  var calculo = (peso+((erroPeso/100)*peso))*(preco/1000) + ((tempo+((erroTempo/100)*tempo))*taxaEnergia*consumo);
 
-  var lucro = calculo*1 + calculo;
-  lucro = lucro.toFixed(2);
+  var precoFinal = (calculo*(lucro/100)) + calculo;
+  precoFinal = precoFinal.toFixed(2);
 
-  $("#resultado").text("R$ "+ lucro);
+  $("#resultado").text("R$ "+ precoFinal);
 })
 
   M.AutoInit();
--- app_calc_3d/www/js/index.js
+++ app_calc_3d/www/js/index.js
@@ -4,14 +4,15 @@ $("#calc").click(function() {
   var tempo = $("#tempo").val();
   var preco = $("#preco-filamento").val();
   var consumo = $("#consumo-impressora").val();
-  var taxa = $("#taxa-energia").val();
+  var taxaEnergia = $("#taxa-energia").val();
   var lucro = $("#lucro").val();
   var erroTempo = $("#erro-tempo").val();
   var erroPeso = $("#erro-peso").val();
 
-  var calculo = (peso+erroPeso)*(preco/1000) + ((tempo+erroTempo)*taxaEnergia*consumo);
+ console.log("o calculo ficou assim: ("+peso+"+("+(erroPeso/100)+"*"+peso+"))*("+preco+"/1000)");
+  var calculo = (peso+((erroPeso/100)*peso))*(preco/1000) + ((tempo+((erroTempo/100)*tempo))*taxaEnergia*consumo);
 
-  var precoFinal = calculo*(lucro/100)) + calculo;
+  var precoFinal = (calculo*(lucro/100)) + calculo;
   precoFinal = precoFinal.toFixed(2);
 
   $("#resultado").text("R$ "+ precoFinal);
